6f95c28c2ec2e0f9370b75886bcf5e32f2250571
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2025-10-11 21:04:17 +0300

    Initial commit

---
+++ pair.js
@@ -0,0 +1,107 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Maher_Zubair,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function SIGMA_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_Maher_Zubair = Maher_Zubair({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "silva", "Mac OS"]
+             });
+             if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Maher_Zubair.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'Silva~' + b64data });
+
+               let SIGMA_MD_TEXT = `
+*SILVA TECH INC*
+*you have used pairing method*
+> YOU HAVE SUCCESSFULLY COMPLETED YOUR FIRST STEP
+> NOW COPY THE SESSION CODE ABOVE AND USE IT IN YOUR PREFERED DEPLOYING SITE
+____________________________________
+╔════◇
+║『 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍』
+
+║ ❒ SILVA: _https://wa.me/254743706010_
+
+╚════════════════════❒
+╔═════◇
+║ 『••• OWNER INFO •••』
+║ ❒ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _https://www.youtube.com/@silvaedits254_
+
+║ ❒ 𝐎𝐰𝐧𝐞𝐫: _https://wa.me/254700143167_
+
+║ ❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://www.whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v_
+> fork the following repo
+║ ❒ Repo 1: _https://github.com/SilvaTechB/silva-md-bot_
+║ ❒ Repo 2: _https://github.com/Sylivanu/s-ilva_
+╚════════════════════╝ 
+ *silva tech inc*
+___________________________________
+
+'_Don't Forget To Give Star To My Repo_`
+ await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_Maher_Zubair.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    SIGMA_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await SIGMA_MD_PAIR_CODE()
+});
+module.exports = router
---
+++ qr.js
@@ -0,0 +1,112 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Maher_Zubair,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function SIGMA_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Maher_Zubair = Maher_Zubair({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
+
+               let SIGMA_MD_TEXT = `
+*SILVA TECH INC*
+*you have used QR method*
+> YOU HAVE SUCCESSFULLY COMPLETED YOUR FIRST STEP
+> NOW COPY THE SESSION CODE ABOVE AND USE IT IN YOUR PREFERED DEPLOYING SITE
+____________________________________
+╔════◇
+║『 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍』
+
+║ ❒ SILVA: _https://wa.me/254743706010_
+
+╚════════════════════❒
+╔═════◇
+║ 『••• OWNER INFO •••』
+║ ❒ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: _https://www.youtube.com/@silvaedits254_
+
+║ ❒ 𝐎𝐰𝐧𝐞𝐫: _https://wa.me/254700143167_
+
+║ ❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://www.whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v_
+> fork the following repo
+║ ❒ Repo 1: _https://github.com/SilvaTechB/silva-md-bot_
+║ ❒ Repo 2: _https://github.com/Sylivanu/s-ilva_
+╚════════════════════╝ 
+ *silva tech inc*
+___________________________________
+
+'_Don't Forget To Give Star To My Repo_`
+	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Maher_Zubair.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					SIGMA_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await SIGMA_MD_QR_CODE()
+});
+module.exports = router
